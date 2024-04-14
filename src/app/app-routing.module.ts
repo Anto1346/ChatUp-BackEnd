e7163924c2922f699d5a6c392af4c3ddbde1edf7
John Doe <163063088+Anto1346@users.noreply.github.com>
2024-04-14 21:33:34 +0200

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ChatComponent } from './components/chat/chat.component';
+
+const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: '/app-login',
+    pathMatch: 'full'
+  },
+  { 
+    path: 'login', 
+    component: LoginComponent 
+  },
+  { 
+    path: 'register', 
+    component: RegisterComponent
+  },
+  { 
+    path: 'forgot-password', 
+    component: ForgotPasswordComponent
+  },
+  { 
+    path: 'chat', 
+    component: ChatComponent 
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
